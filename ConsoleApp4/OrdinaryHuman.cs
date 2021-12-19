bf61aa5200560406bb3dd9988630a7fea163025c
Ahmed Hafez <Ahmed.abdelghany.Hafez@gmail.com>
2021-12-19 22:33:52 +0200

    Add project files.

---
+++ ConsoleApp4/OrdinaryHuman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    sealed class OrdinaryHuman
+    {
+        private int age;
+        int weight;
+
+        public int Weight { get { return weight; } set { weight = value; } }
+        public OrdinaryHuman(int weight)
+        {
+            this.weight = weight;
+        }
+        public void GoToWork() { /* code to go to work */ }
+        public void PayBills() { /* code to pay bills */ }
+    }
+
+    static class AmazeballsSerum
+    {
+        public static string BreakWalls(this OrdinaryHuman human, double wallDensity)
+        {
+            return $"I broke {human.Weight} through a wall of {wallDensity} density.";
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrdinaryHuman human = new OrdinaryHuman(100);
+
+            Console.WriteLine(human.BreakWalls(44.8));
+
+
+           string sentence = AmazeballsSerum.BreakWalls(human, 88.5);
+
+            Console.WriteLine(sentence);
+
+            List<int> list = new List<int>();
+            list.
+
+            IEnumerable<int> rangeList = Enumerable.Range(0, sentence.Length);
+
+            foreach (int i in rangeList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
